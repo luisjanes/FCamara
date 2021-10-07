2a2525cdcc80bfe26fa581779ba3c9531a5255e5
Luis Felipe Janes <luisf.janes@gmail.com>
2021-10-07 09:38:12 -0300

    Atualização do código

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 let mesasOcupadas = []
 
-function pegar(){
-    let mesa = document.getElementById("mesas").value
-    let escritorio = document.querySelector("input[name='escritorio']:checked").value
-    let data = document.getElementById('inputDate').value
-    let func = document.getElementById('inputCodFunc').value
-    let verificarMesa = false
-    let verificarData = false
-    let verificarFunc = false
-    let verificarEscri = false
-    if(mesa == '' || escritorio == '' || data == '' || func == ''){
-        alert('Por favor preencha todos os campos')
-        return
-    }
-    
-    marcar(escritorio, mesa, data, func)
-}
-function marcar(escritorio, mesa, data, func){
-    mesasOcupadas.push({escritorio: escritorio, mesas: mesa, datas: data, funcionario: func})
+function Submit()
+{
+	let table = document.getElementById("mesas");
+	let office = document.querySelector("input[name='escritorio']:checked");
+	let date = document.getElementById('inputDate');
+	let func = document.getElementById('inputCodFunc');
+
+	let newEntry =
+	{
+		table: table.value,
+		office: office.value,
+		date: date.value,
+		func: func.value,
+	};
+
+	if(newEntry.table == '' || newEntry.office == '' || newEntry.date == '' || newEntry.func == '')
+	{
+		alert(`Complete todos os espaços em branco`);
+		return;
+	}
+
+	let isOk = true;
+	for(let i = 0; i < mesasOcupadas.length; i++)
+	{
+		let currentEntry = mesasOcupadas[i];
+		if(currentEntry.func == newEntry.func && currentEntry.date == newEntry.date) {
+			isOk = false;
+		}
+		if(currentEntry.office == newEntry.office && currentEntry.table == newEntry.table && currentEntry.date == newEntry.date) {
+			isOk = false;
+		}
+	}
+
+	if(!isOk)
+	{
+		alert(`Escolha outra data ou mesa.`);
+		return;
+	}
+
+	mesasOcupadas.push(newEntry);
+	alert(`Você agendou a ${newEntry.table}, para o dia ${newEntry.date}, no escritório de ${newEntry.office}`);
     console.log(mesasOcupadas)
-    console.log(mesasOcupadas.includes(mesa))
-    alert(`Você agendou a ${mesa}, para o dia ${data}, no escritório de ${escritorio}`)
-    inputCodFunc.value = ''
-    inputDate.value = ''
-    mesas.value = ''
+	table.value = '';
+	date.value = '';
+	func.value = '';
 }
